97ed6c4074115789627a0bf695571c8b325a024c
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2021-05-17 15:01:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express'); // Obviously, we need Express.
+const pug = require('pug'); // And we couldn't render Pug without Pug.
+require('jstransformer-markdown-it'); // This needs to be installed for Pug to use it.
+
+const app = express();
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.send(pug.renderFile('public/index.pug')); // We use pug.renderFile to render a file.
+});
+
+// Add more URLs and Pug files here...
+
+app.listen(8080, function() {
+    console.log('Server up!');
+});
+db.set("INT", "32").then(() => {});
+db.get("INT32").then(value => {});
+db.list().then(keys => {});
+const Database = require("@replit/database")const db = new Database()

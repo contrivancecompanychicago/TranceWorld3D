ac5a9576c08c2dd0975fcd26680a12e2cb0df8dd
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2021-08-13 11:13:04 -0500

    Add files via upload

---
+++ MapofUrbanMarket.js
@@ -0,0 +1,159 @@
+ {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -87.66102208,
+          41.83322271
+        ]
+      },
+      properties: {
+        ADDRESS: "3333 S Iron Street ",
+        LOCATION: "(41.83322271, -87.66102208)"
+      }
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -87.64044485,
+          41.89661725
+        ]
+      },
+      properties: {
+        ADDRESS: "444 W Chicago ",
+        LOCATION: "(41.89661725, -87.64044485)"
+      }
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -87.61847785,
+          41.76564492
+        ]
+      },
+      properties: {
+        ADDRESS: "71st & Prairie",
+        LOCATION: "(41.76564492, -87.61847785)"
+      }
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -87.66929524,
+          41.7885099
+        ]
+      },
+      properties: {
+        ADDRESS: "58th & Wood",
+        LOCATION: "(41.7885099, -87.66929524)"
+      }
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -87.65442626,
+          41.86714301
+        ]
+      },
+      properties: {
+        ADDRESS: "1100 W Roosevelt ",
+        LOCATION: "(41.86714301, -87.65442626)"
+      }
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -87.63587876,
+          41.81444995
+        ]
+      },
+      properties: {
+        ADDRESS: "4400 S Stewart ",
+        LOCATION: "(41.81444995, -87.63587876)"
+      }
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -87.70648607,
+          41.88831522
+        ]
+      },
+      properties: {
+        ADDRESS: "400 N Kedzie ",
+        LOCATION: "(41.88831522, -87.70648607)"
+      }
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -87.62786143,
+          41.79090671
+        ]
+      },
+      properties: {
+        ADDRESS: "5700 S Perry ",
+        LOCATION: "(41.79090671, -87.62786143)"
+      }
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -87.62637112,
+          41.81460224
+        ]
+      },
+      properties: {
+        ADDRESS: "4400 S State",
+        LOCATION: "(41.81460224, -87.62637112)"
+      }
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -87.5513461,
+          41.7337305
+        ]
+      },
+      properties: {
+        ADDRESS: "89th & Commercial",
+        LOCATION: "(41.7337305, -87.5513461)"
+      }
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -87.60742878,
+          41.65925479
+        ]
+      },
+      properties: {
+        ADDRESS: "600 E 130th St ",
+        LOCATION: "(41.65925479, -87.60742878)"
+      }
+    }
+  ]
+}
